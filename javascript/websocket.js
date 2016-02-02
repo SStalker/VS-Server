0d174775ddfc54496470853f42ce5bec3742d56c
Lukas Hannigbrinck <lukas@bluepanel.org>
2016-02-03 00:05:28 +0100

    changed the web dl for jquery to a newer local file and implementet first steps for registration in web client

--- javascript/websocket.js
+++ javascript/websocket.js
@@ -1,5 +1,10 @@
 //create a new websocket
-var websocket = new WebSocket("ws://iknowit.ddns.net:1919");
+var url = "127.0.0.1";
+var urll = "192.168.2.107";
+
+//var websocket = new WebSocket("ws://iknowit.ddns.net:1919");
+//var websocket = new WebSocket("ws://192.168.178.31:1919");
+var websocket = new WebSocket("ws://"+ url +":1919");
 var websocketOk;
 websocket.onopen = function(event){
 	websocketOk = true;
@@ -37,9 +42,13 @@ processMessage = function(data){
 	//do something on the data....
 	var dataArray = JSON.parse(data);
 	//get the message type
-	var type = dataArray.type;
-	if(type === "hmtl"){
-		//insert html to its target
+	if(dataArray.response !== 'undefined'){
+		var type = dataArray.response;
+		console.log(dataArray);
+		if(type === "registersite"){
+			console.log(dataArray.message);
+			//$(".wrapper").html(dataArray.message);
+		}
 	}
 };
 
@@ -48,17 +57,29 @@ $(document).ready(function(){
 
 	//Funktion to seriallize login form data
 	//ToDo: Add encryption to password and define the send message in documentation
-	$("form").submit(function(event){
+	$("#login").submit(function(event){
 		var logindata = {
-			type: "logindata",
-			date: Date.now(),
+			request: "login",
 		};
 		$(this).serializeArray().map(function(x){logindata[x.name] = x.value;});
+
 		console.log( JSON.stringify(logindata));
-
-		//sendMessage(logindata);
-
+		sendMessage(logindata);
 		event.preventDefault();
+	});
+
+	$("#register").click(function(){
+		console.log("Load template");
+		/*$(".wrapper").load("webclient-templates/register.html", function( response, status, xhr ) {
+			if ( status == "error" ) {
+			    	console.log("Sorry but there was an error: "+ xhr.status + " " + xhr.statusText );
+			}
+		})*/
+		var data = {
+			request: "registersite"
+		}
+
+		sendMessage(data);
 	});
 });
 
