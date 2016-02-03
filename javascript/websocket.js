ab5f31d4fe860bb23b98a6cbf8546d7bc0678b61
Lukas Hannigbrinck <lukas@bluepanel.org>
2016-02-03 16:38:45 +0100

    extend functionality, implemented new way to creat json object and changed acces to parsed json array

--- javascript/websocket.js
+++ javascript/websocket.js
@@ -8,6 +8,7 @@ var websocket = new WebSocket("ws://"+ url +":1919");
 var websocketOk;
 websocket.onopen = function(event){
 	websocketOk = true;
+	setStatus();
 	//DEBUGG
 	console.log("open");
 };
@@ -15,6 +16,8 @@ websocket.onopen = function(event){
 websocket.onerror = function (error) {
 
 	//If the websocket is closed due some issues set websocketOk to false
+	websocketOk = false;
+	setStatus();
 	//DEBUGG
 	console.log('WebSocket Error ' + error);
 };
@@ -27,6 +30,14 @@ websocket.onmessage = function(event){
 	console.log(event.data);
 };
 
+//Set Connection Status
+setStatus = function(){
+	if(websocketOk === true ){
+		$("#insert-status").html("Mit Server Verbunden!");
+	}else{
+		$("#insert-status").html("Nicht mit Server Verbunden!");
+	}
+};
 
 //Send a message
 sendMessage = function(msg){
@@ -34,6 +45,7 @@ sendMessage = function(msg){
 		websocket.send(JSON.stringify(msg));
 	}else{
 		//Error -->  do something
+		console.log("no open websocket connection");
 	}
 };
 
@@ -46,8 +58,8 @@ processMessage = function(data){
 		var type = dataArray.response;
 		console.log(dataArray);
 		if(type === "registersite"){
-			console.log(dataArray.message);
-			$(".wrapper").html(dataArray.message);
+			console.log(dataArray.Value1.message);
+			$(".wrapper").html(dataArray.Value1.message);
 		}
 	}
 };
@@ -57,29 +69,48 @@ $(document).ready(function(){
 
 	//Funktion to seriallize login form data
 	//ToDo: Add encryption to password and define the send message in documentation
-	$("#login").submit(function(event){
+	$('.wrapper').on('submit',"#login", function(event){
 		var logindata = {
 			request: "login",
+			values: {
+			}
 		};
-		$(this).serializeArray().map(function(x){logindata[x.name] = x.value;});
+
+		$(this).serializeArray().map(function(x){logindata.values[x.name] = x.value;});
 
 		console.log( JSON.stringify(logindata));
 		sendMessage(logindata);
 		event.preventDefault();
 	});
 
-	$("#register").click(function(){
+	$('.wrapper').on('click', "#register" ,function(){
 		console.log("Load template");
-		/*$(".wrapper").load("webclient-templates/register.html", function( response, status, xhr ) {
+		$(".wrapper").load("webclient-templates/register.html", function( response, status, xhr ) {
 			if ( status == "error" ) {
 			    	console.log("Sorry but there was an error: "+ xhr.status + " " + xhr.statusText );
+				$(".wrapper").html("<h3>Konnte Template nicht laden</h3>\n" +
+							"<a href=\"newIndex.html\">Zurück</a>");
 			}
-		})*/
+		})
+		/*
 		var data = {
 			request: "registersite"
 		}
-
 		sendMessage(data);
+		*/
+	});
+
+	$('.wrapper').on('click', '#back',function(){
+		console.log("clicked back");
+		$(".wrapper").load("webclient-templates/login.html", function( response, status, xhr ) {
+			if ( status == "error" ) {
+			    	console.log("Sorry but there was an error: "+ xhr.status + " " + xhr.statusText );
+				$(".wrapper").html("<div class=\"error\">\n" 			+
+							"<h3>Konnte Template nicht laden</h3>\n"+
+							"<a href=\"newIndex.html\">Zurück</a>"	+
+							"</div>");
+			}
+		})
 	});
 });
 
